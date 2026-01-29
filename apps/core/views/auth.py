84d1871038427f987b0f06d59fdef19dbc40a9ce
hexa01 <sushanpoudel80@gmail.com>
2026-01-29 20:59:54 +0545

    refactor(views): update views to modular structure creating a directory

---
+++ apps/core/views/auth.py
@@ -0,0 +1,8 @@
+def register_view():
+    pass
+
+def login_view():
+    pass
+
+def logout_view():
+    pass
--- apps/core/views/home.py
+++ apps/core/views/home.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import logging
+
 # Create your views here.
 logger = logging.getLogger(__name__)
 def home(request):
